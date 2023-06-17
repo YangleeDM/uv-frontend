54e6dc856ec351d91e81c8a43184c18e6d52f77e
YangleeDM <yangleedm@gmail.com>
2023-06-17 10:05:49 -0600

    Generated input component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,14 +3,15 @@ import {PhraseComponent} from "./phrase-component/phrase.component";
 import {IonicModule} from "@ionic/angular";
 import {CommentComponent} from "./comment/comment.component";
 import {PhrsarioToolbarComponent} from "./phrsario-toolbar/phrsario-toolbar.component";
+import {PhrasarioInputComponent} from "./phrasario-input/phrasario-input.component";
 
 
 @NgModule({
-  declarations : [PhraseComponent, CommentComponent, PhrsarioToolbarComponent],
+  declarations : [PhraseComponent, CommentComponent, PhrsarioToolbarComponent, PhrasarioInputComponent],
   imports: [
     IonicModule
   ],
-  exports : [PhraseComponent, CommentComponent, PhrsarioToolbarComponent]
+  exports : [PhraseComponent, CommentComponent, PhrsarioToolbarComponent, PhrasarioInputComponent]
 })
 
 export class ComponentsModule {}
---
+++ src/app/components/phrasario-input/phrasario-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'phrasario-input',
+  templateUrl: './phrasario-input.component.html',
+  styleUrls: ['./phrasario-input.component.scss'],
+})
+export class PhrasarioInputComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
